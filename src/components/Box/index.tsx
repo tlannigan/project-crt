22b834f64153f9d45850a36956aa5d29891d96c3
tlannigan <tristanlannigan@gmail.com>
2026-07-09 22:14:57 -0600

    Add box-shadow to Box component

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -3,7 +3,7 @@ import { twMerge } from 'tailwind-merge';
 
 export default function Box({ title, className, children }: ComponentProps<'div'>) {
   return (
-    <div className={twMerge('relative border-2 border-foreground px-6 py-5', className)}>
+    <div className={twMerge('relative border-2 border-foreground px-6 py-5 shadow-glow', className)}>
       {title && <span className="absolute -top-3.5 left-3 bg-background px-3">{title}</span>}
       {children}
     </div>
