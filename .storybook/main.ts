5417c893515c6e22af6574011ed25174440fa372
tlannigan <tristanlannigan@gmail.com>
2026-09-23 21:19:37 -0600

    Add MCP servers for Next, Storybook, and Playwright

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,8 @@ const config: StorybookConfig = {
     '@storybook/addon-vitest',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
-    '@storybook/addon-themes'
+    '@storybook/addon-themes',
+    '@storybook/addon-mcp'
   ],
 
   framework: '@storybook/nextjs-vite',
